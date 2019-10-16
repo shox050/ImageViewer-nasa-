dde459596eb59067929793438d7c2ff7a44fd50a
Vladimir Yakutin <va.yakutin@gmail.com>
2019-10-16 15:39:59 +0300

    Initial commit.

---
+++ ImageViewer (nasa)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageViewer (nasa)
+//
+//  Created by Vladimir on 16.10.2019.
+//  Copyright © 2019 VladimirYakutin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
